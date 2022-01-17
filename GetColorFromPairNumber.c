fb064d773d4b45b9db1363e3ef0331557922bcd5
AshidaSageer <91050341+AshidaSageer@users.noreply.github.com>
2022-01-17 21:03:19 +0530

    Update GetColorFromPairNumber.c

--- GetColorFromPairNumber.c
+++ GetColorFromPairNumber.c
@@ -1,1 +1,13 @@
 
+#include "declarations.h"
+#include "GetColorFromPairNumber.h"
+
+ColorPair GetColorFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = 
+        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor =
+        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorPair;
+}
