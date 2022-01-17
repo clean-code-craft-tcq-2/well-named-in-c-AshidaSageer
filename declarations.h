ecc7647219314b70054aeaf168505795c925616b
AshidaSageer <91050341+AshidaSageer@users.noreply.github.com>
2022-01-17 18:49:42 +0530

    Update declarations.h

--- declarations.h
+++ declarations.h
@@ -1,17 +1,11 @@
-extern enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-extern enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+extern enum MajorColor ;
+extern enum MinorColor ;
 
-extern const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-extern int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-extern const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-extern const int MAX_COLORPAIR_NAME_CHARS = 16;
-extern int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+extern const char* MajorColorNames[] ;
+extern int numberOfMajorColors ;
+extern const char* MinorColorNames[] ;
+extern const int MAX_COLORPAIR_NAME_CHARS ;
+extern int numberOfMinorColors ;
 
 extern typedef struct {
     enum MajorColor majorColor;
