c9666527afe27603249465d5bed1c239b7950515
AshidaSageer <91050341+AshidaSageer@users.noreply.github.com>
2022-01-17 21:08:06 +0530

    Update GetColorFromPairNumber.h

--- GetColorFromPairNumber.h
+++ GetColorFromPairNumber.h
@@ -1,2 +1,7 @@
-GetColorFromPairNumber
+#ifndef GETCOLORFROMPAIRNUMBER_DOT_H
+#define GETCOLORFROMPAIRNUMBER_DOT_H
+
+#include "declarations.h"
 ColorPair GetColorFromPairNumber(int pairNumber);
+
+#endif
