0da703681bf198556fa446dded692e5244526271
AshidaSageer <91050341+AshidaSageer@users.noreply.github.com>
2022-01-17 19:06:13 +0530

    Update main.c

--- main.c
+++ main.c
@@ -20,13 +20,13 @@ int numberOfMinorColors =
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
-} ColorPair;*/
+} ColorPair;
 
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
         MajorColorNames[colorPair->majorColor],
         MinorColorNames[colorPair->minorColor]);
-}
+}*/
 
 ColorPair GetColorFromPairNumber(int pairNumber) {
     ColorPair colorPair;
