59d0827e07f04d5ab739190e663fdf3ff6832437
AshidaSageer <91050341+AshidaSageer@users.noreply.github.com>
2022-01-17 18:54:53 +0530

    Update declarations.c

--- declarations.c
+++ declarations.c
@@ -14,7 +14,4 @@ const int MAX_COLORPAIR_NAME_CHARS = 16;
 int numberOfMinorColors =
     sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
-typedef struct {
-    enum MajorColor majorColor;
-    enum MinorColor minorColor;
-} ColorPair;
+
