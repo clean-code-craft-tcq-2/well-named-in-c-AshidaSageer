2f2b8a137eda0118c55c5a8f6918a851cadcb182
AshidaSageer <91050341+AshidaSageer@users.noreply.github.com>
2022-01-18 18:07:40 +0530

    Update GetPairNumberFromColor.c

--- GetPairNumberFromColor.c
+++ GetPairNumberFromColor.c
@@ -1,3 +1,6 @@
+#include "declarations.h"
+#include "GetPairNumberFromColor.h"
+
 int GetPairNumberFromColor(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors +
             colorPair->minorColor + 1;
