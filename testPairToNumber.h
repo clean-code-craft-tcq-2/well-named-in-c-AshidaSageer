3592a2f455c927f1a83b64edf0056a58b13f1cc3
AshidaSageer <91050341+AshidaSageer@users.noreply.github.com>
2022-01-18 19:47:37 +0530

    Update testPairToNumber.h

--- testPairToNumber.h
+++ testPairToNumber.h
@@ -1,1 +1,9 @@
+#ifndef TESTPAIRTONUMBER_DOT_H
+#define TESTPAIRTONUMBER_DOT_H
 
+#include "declarations.h"
+void testNumberToPair(int pairNumber,
+    enum MajorColor expectedMajor,
+    enum MinorColor expectedMinor);
+
+#endif
