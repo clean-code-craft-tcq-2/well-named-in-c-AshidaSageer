49a4e4c8ad993cb5df0861d6fed845e47539727f
AshidaSageer <91050341+AshidaSageer@users.noreply.github.com>
2022-01-18 23:21:37 +0530

    Update printManual.c

--- printManual.c
+++ printManual.c
@@ -6,19 +6,9 @@
 
 void printManual(void)
 {
- /*ColorPair colorPair ;
- char colorPairName[MAX_COLORPAIR_NAME_CHARS];
- int pairNumber = 0; 
- //for (int pairNumber = 0; pairNumber <= 25; pairNumber ++)
-    {
-     colorPair = GetColorFromPairNumber(pairNumber);
-     ColorPairToString(&colorPair, &colorPairName[0]);
-     printf("Pair No:%d \t --> %s\n",pairNumber, colorPairName);
-    }*/
  ColorPair colorPair ;
- char colorPairName[MAX_COLORPAIR_NAME_CHARS];
- int pairNumber = 1; 
- //for (int pairNumber = 0; pairNumber <= 25; pairNumber ++)
+ char colorPairName[MAX_COLORPAIR_NAME_CHARS]; 
+ for (int pairNumber = 1; pairNumber <= 25; pairNumber ++)
     {
      colorPair = GetColorFromPairNumber(pairNumber);
      ColorPairToString(&colorPair, &colorPairName[0]);
